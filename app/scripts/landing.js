31da82479b70e3c9f211d8a2f2cee72184e3e832
Ha Na Gill <oneway.first@gmail.com>
2014-12-23 05:14:50 +0000

    Added jQuery animation to landing page

---
+++ app/scripts/landing.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+	$('.hero-content h3').click(function(){	
+	var subText = $(this).text();
+	$(this).text(subText + "! ");
+	});
+
+	var onHoverAction = function(event){
+		console.log('Hover action triggered.');
+		$(this).animate({'margin-top': '10px'});
+	};
+
+	var offHoverAction = function(event){
+		console.log('Off-Hover action triggered.');
+		$(this).animate({'margin-top': '0px'});
+	};
+
+	$('.selling-points .point').hover(onHoverAction, offHoverAction);
+
+});
