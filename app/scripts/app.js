49ea13bcd96b913027c902062a471502cd502a33
Ha Na Gill <oneway.first@gmail.com>
2015-02-11 18:48:43 +0000

    Made next and previous buttons on player-bar work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -113,6 +113,11 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
 }]);
 
   blocJams.service('SongPlayer', function(){
+
+    var trackIndex = function(album, song){
+      return album.songs.indexOf(song);
+    };
+
     return {
       currentSong: null,
       currentAlbum: null,
@@ -124,6 +129,22 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
       pause: function() {
         this.playing = false;
       },
+      next: function(){
+        var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+        currentTrackIndex++;
+        if(currentTrackIndex >= this.currentAlbum.songs.length){
+          currentTrackIndex = 0;
+        }
+        this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+      },
+      previous: function(){
+        var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+        currentTrackIndex--;
+        if(currentTrackIndex < 0){
+          currentTrackIndex = this.currentAlbum.songs.length - 1;
+        }
+        this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+      },
       setSong: function(album, song){
         this.currentAlbum = album;
         this.currentSong = song;
@@ -131,8 +152,6 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
     };
 
   });
-
-
 
 
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -383,6 +383,11 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
 }]);
 
   blocJams.service('SongPlayer', function(){
+
+    var trackIndex = function(album, song){
+      return album.songs.indexOf(song);
+    };
+
     return {
       currentSong: null,
       currentAlbum: null,
@@ -394,6 +399,22 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
       pause: function() {
         this.playing = false;
       },
+      next: function(){
+        var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+        currentTrackIndex++;
+        if(currentTrackIndex >= this.currentAlbum.songs.length){
+          currentTrackIndex = 0;
+        }
+        this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+      },
+      previous: function(){
+        var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+        currentTrackIndex--;
+        if(currentTrackIndex < 0){
+          currentTrackIndex = this.currentAlbum.songs.length - 1;
+        }
+        this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+      },
       setSong: function(album, song){
         this.currentAlbum = album;
         this.currentSong = song;
@@ -401,8 +422,6 @@ blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($s
     };
 
   });
-
-
 
 
 
